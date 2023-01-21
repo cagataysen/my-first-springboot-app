38cec11d54cb00ea8b72154e21194a5e7f446c39
[cagataysen] <drosen_26@hotmail.com>
2023-01-22 00:21:20 +0300

    Spring Rest API

---
+++ src/main/java/com/javaguides/springboot/myfirstspringbootapp/MyFirstSpringbootAppApplication.java
@@ -0,0 +1,14 @@
+package com.javaguides.springboot.myfirstspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyFirstSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringbootAppApplication.class, args);
+	}
+
+}
